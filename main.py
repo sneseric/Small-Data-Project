c41d7dd1826538837b43fa46a1585e397476c433
sneseric <eklausner@mail.valenciacollege.edu>
2024-02-25 19:42:33 -0500

    Update script

--- main.py
+++ main.py
@@ -20,31 +20,37 @@ api = NewsApiClient(api_key=api_key)
 @app.route('/')
 def index():
     # Fetch headlines for Orlando, FL
-    #top_headlines = api.get_everything(q='Orlando + "Florida"', language='en', sort_by='relevancy')
     top_headlines = api.get_everything(
         q='(Orlando AND Florida) OR (Altamonte Springs AND Florida) OR (Orange County AND Florida)',
         language='en',
-        #sort_by='relevancy'
-        sort_by='publishedAt'
+        sort_by='relevancy'   # Other sorting options: 'popularity', 'publishedAt'
     )
 
     # Count articles related to theme parks
-    theme_park_articles = sum(
-        'Disney World' in article['title'] or 'Universal Studios' in article['title'] for article in
-        top_headlines['articles'])
-    other_articles = len(top_headlines['articles']) - theme_park_articles
+    theme_park_articles = [article for article in top_headlines['articles'] if 'Disney World' in article['title'] or 'Universal Studios' in article['title']]
 
-    # Create pie chart
+    # Count articles related to DeSantis
+    desantis_articles = [article for article in top_headlines['articles'] if 'DeSantis' in article['title']]
+
+    # Updated sports-related articles count
+    sports_keywords = ['football', 'basketball', 'baseball', 'soccer', 'tennis', 'golf', 'LeBron James', 'Lionel Messi',
+                       'Serena Williams', 'Tom Brady', 'Tiger Woods', 'Travis Kelce']
+    sports_articles = [article for article in top_headlines['articles'] if
+                       any(keyword in article['title'] for keyword in sports_keywords)]
+
+    # Count other articles
+    other_articles = len(top_headlines['articles']) - len(theme_park_articles) - len(desantis_articles) - len(
+        sports_articles)
+
+    # Create pie chart for news categories
     plt.figure(figsize=(6, 6))
-    plt.pie([theme_park_articles, other_articles], labels=['Theme Parks', 'Other'], autopct='%1.1f%%')
+    categories = ['Theme Parks', 'DeSantis', 'Sports', 'Other']
+    articles_count = [len(theme_park_articles), len(desantis_articles), len(sports_articles), other_articles]
+    plt.pie(articles_count, labels=categories, autopct='%1.1f%%')
     plt.title('Article Categories')
-
-    # Save pie chart to a BytesIO object
     buf = io.BytesIO()
     plt.savefig(buf, format='png')
     buf.seek(0)
-
-    # Convert BytesIO object to base64 string
     pie_chart = base64.b64encode(buf.read()).decode('utf8')
 
     # Pass articles to template
